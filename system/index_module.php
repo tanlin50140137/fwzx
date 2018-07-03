5447a3347cd2714662d622862ec46620343970e6
tanlin50140137 <39691304+tanlin50140137@users.noreply.github.com>
2018-07-03 11:02:08 +0800

    fu
    fu

---
+++ system/index_module.php
@@ -0,0 +1,14 @@
+<?php
+header('content-type:text/html;charset=utf-8');
+/**
+ * 所有模块必须写在函数里
+ * */
+#主页面 , Ready to start work (准备开始工作)
+function index()
+{
+	session_start();
+	#公共文件内容
+	include 'subject/'.getThemeDir().'/common.php';
+	
+	require 'subject/'.getThemeDir().'/template/'.__FUNCTION__.'.html';
+}
